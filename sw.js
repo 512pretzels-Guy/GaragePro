b6bc2ad0452cc1f7b2f9e53779a9c16bec491008
512pretzels-Guy <512pretzels@gmail.com>
2026-04-22 22:51:26 -0400

    sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,15 @@
-const CACHE_NAME = 'garage-pro-v1';
-const ASSETS = [
-  './Garage Pro32.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'garage-pro-v2';
 
+// We catch the install event and force it to complete
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+// We must have a fetch handler for the 'Install' button to appear
 self.addEventListener('fetch', (event) => {
-    // This can be empty, but the listener MUST exist
     event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
     );
 });

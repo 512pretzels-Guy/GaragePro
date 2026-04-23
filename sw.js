70081cdef71be2d9df53b48a574eb18860d35ef4
512pretzels-Guy <512pretzels@gmail.com>
2026-04-22 23:06:15 -0400

    sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,25 @@
-const CACHE_NAME = 'garage-pro-v2';
+const CACHE_NAME = 'garage-pro-v4';
+const ASSETS = [
+  './GaragePro.html',
+  './manifest.json'
+];
 
-// We catch the install event and force it to complete
+// Installs and caches the core files
 self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
     self.skipWaiting();
 });
 
-// We must have a fetch handler for the 'Install' button to appear
+// Activates the service worker
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+// Crucial: Intercepts network requests to make Chrome see this as a valid PWA
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         fetch(event.request).catch(() => {
